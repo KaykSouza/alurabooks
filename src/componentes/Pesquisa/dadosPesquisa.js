84bb0cff44a1580643b960e5ee143ebda1177a1d
KaykSouza <kayksmr@gmail.com>
2024-01-23 10:01:19 -0300

    Atualizando os estados com o onBlur

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,8 @@
+import Livro from '../../imagens/livro.png'
+
+export const livros = [
+    {nome: 'Liderança em Design', id: 1, src: Livro},
+    {nome: 'JavaScript Acertivo', id: 2, src: Livro},
+    {nome: 'Cangaceiro em JavaScript', id: 3, src: Livro},
+    {nome: 'Aventureiros do Java', id: 4, src: Livro},
+]
